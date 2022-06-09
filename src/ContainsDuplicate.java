153cb5d634340bdfb96e8e91ad85ff5d8cc24517
Tsetsenbileg <tsetsenbileg0413@gmail.com>
2022-06-09 16:38:47 +0800

    Initial commit

---
+++ src/ContainsDuplicate.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+
+        int[] a = {1,2,3,1};
+        int[] b= {1,2,3,4};
+
+        ContainsDuplicate c = new ContainsDuplicate();
+
+        boolean result = c.containsDuplicate(a);
+        System.out.println(result);
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int n:nums){
+
+           if(set.contains(n)){
+               return true;
+           }
+           set.add(n);
+        }
+        return false;
+    }
+}
---
+++ src/MaximumSubarray.java
@@ -0,0 +1,30 @@
+public class MaximumSubarray {
+
+    public static void main(String[] args) {
+
+        int[] a = {-2,1,-3,4,-1,2,1,-5,4};
+
+        MaximumSubarray b = new MaximumSubarray();
+
+        System.out.println(b.maxSubArray(a));
+    }
+
+    public int maxSubArray(int[] nums) {
+
+        int max_sum = Integer.MIN_VALUE ;
+        int temp_sum = 0;
+
+        for(int n = 0 ; n< nums.length; n++){
+
+            temp_sum += nums[n];
+            if(max_sum < temp_sum) {
+                max_sum = temp_sum;
+            }
+            if(temp_sum < 0){
+                temp_sum = 0;
+            }
+        }
+        return max_sum;
+
+    }
+}
