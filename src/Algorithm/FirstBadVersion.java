526c894a1d6dc33f209ade2f55c3c41caad386e8
Tsetsenbileg <tsetsenbileg0413@gmail.com>
2022-10-18 16:47:17 +0800

    thinking...

--- src/Algorithm/FirstBadVersion.java
+++ src/Algorithm/FirstBadVersion.java
@@ -3,5 +3,16 @@ package Algorithm;
 public class FirstBadVersion {
 
 
+    public static void main(String[] args) {
+        FirstBadVersion fb = new FirstBadVersion();
+
+        fb.firstBadVersion(6);
+    }
+    public int firstBadVersion(int n) {
+
+
+
+        return 1;
+    }
 
 }
---
+++ src/Data_Structure/MergeSortedArray.java
@@ -0,0 +1,15 @@
+package Data_Structure;
+
+public class MergeSortedArray {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    }
+
+}
